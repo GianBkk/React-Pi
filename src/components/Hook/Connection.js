6ef1c55498a5b4087e883bc47b917c93d13a6485
GianBkk <gian200308@gmail.com>
2021-05-03 11:55:02 +0200

    Change Connection.js

--- src/components/Hook/Connection.js
+++ src/components/Hook/Connection.js
@@ -4,9 +4,9 @@ import { Card, Button, Form, Input, Row, Col } from 'antd';
 const Connection = ({ connect, disconnect, connectBtn }) => {
   const [form] = Form.useForm();
   const record = {
-    host: 'broker.emqx.io',
+    host: '192.168.0.14',
     clientId: `mqttjs_ + ${Math.random().toString(16).substr(2, 8)}`,
-    port: 8083,
+    port: 1883,
   };
   const onFinish = (values) => {
     const { host, clientId, port, username, password } = values;
